d66abf3ca91c0c94d2c117a667ca3cff92ab351f
Camilo <cammilo.grimmaldo@hotmail.com>
2020-03-31 13:06:41 -0600

    LogService

---
+++ GestionFC/Services/LogService.cs
@@ -0,0 +1,89 @@
+﻿using GestionFC.Models.Log;
+using GestionFC.Models.Share;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionFC.Services
+{
+    public class LogService
+    {
+
+        private readonly HttpClient _client;
+
+        public LogService()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<LogSistemaResponseModel> LogSistema(LogSistemaModel log, string accessToken)
+        {
+            var logResponse = new LogSistemaResponseModel();
+            try
+            {
+                var uri = new Uri(App.BaseUrlApi + "api/SetLogSistema");
+                var json = JsonConvert.SerializeObject(log);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = null;
+                _client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+                response = await _client.PostAsync(uri, content);
+
+                response.EnsureSuccessStatusCode();
+
+                string jsonResult = await response.Content.ReadAsStringAsync();
+                logResponse = JsonConvert.DeserializeObject<LogSistemaResponseModel>(jsonResult);
+
+                return logResponse;
+            }
+            catch (Exception ex)
+            {
+                logResponse.ResultadoEjecucion = new ResultadoEjecucion()
+                {
+                    EjecucionCorrecta = false,
+                    FriendlyMessage = "Ocurrió un error",
+                    ErrorMessage = ex.Message
+                };
+                return logResponse;
+            }
+
+        }
+
+        public async Task<LogErrorResponseModel> LogError(LogErrorModel log, string accessToken)
+        {
+            var logResponse = new LogErrorResponseModel();
+            try
+            {
+                var uri = new Uri(App.BaseUrlApi + "api/SetLogError");
+                var json = JsonConvert.SerializeObject(log);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = null;
+                _client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+                response = await _client.PostAsync(uri, content);
+
+                response.EnsureSuccessStatusCode();
+
+                string jsonResult = await response.Content.ReadAsStringAsync();
+                logResponse = JsonConvert.DeserializeObject<LogErrorResponseModel>(jsonResult);
+
+                return logResponse;
+            }
+            catch (Exception ex)
+            {
+                logResponse.ResultadoEjecucion = new ResultadoEjecucion()
+                {
+                    EjecucionCorrecta = false,
+                    FriendlyMessage = "Ocurrió un error",
+                    ErrorMessage = ex.Message
+                };
+                return logResponse;
+            }
+
+        }
+
+    }
+}
