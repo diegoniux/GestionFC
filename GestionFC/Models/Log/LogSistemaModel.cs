9c38d213a8a81b3a7d2f443589231c3759471685
Camilo <cammilo.grimmaldo@hotmail.com>
2020-03-31 12:16:19 -0600

    Se agrega el LogSistema y LogError

---
+++ GestionFC/Models/Log/LogSistemaModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionFC.Models.Login
+{
+    public class LogSistemaModel
+    {
+        public int IdAccion { get; set; }
+        public int IdPantalla { get; set; }
+        public int Usuario { get; set; }
+        public string Dispositivo { get; set; }
+    }
+}
---
+++ GestionFC/Models/Log/LogSistemaResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GestionFC.Models.Share;
+
+namespace GestionFC.Models.Login
+{
+    class LogSistemaResponseModel
+    {
+        public ResultadoEjecucion ResultadoEjecucion { get; set; }
+    }
+}
