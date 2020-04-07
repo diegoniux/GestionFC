016d5d604619966e7ae7be4a148bb811fa1932e2
Richi Garza <richi@conglom-osoft.net>
2020-04-06 20:58:58 -0500

    Login

--- GestionFC/Views/LoginPage.xaml.cs
+++ GestionFC/Views/LoginPage.xaml.cs
@@ -31,7 +31,7 @@ namespace GestionFC
 
             //Obtenemos el valor del usuario recordado (ne caso de que exista) para mostrarlo en el entry
             int UserRemember = 0;
-            App.Database.GetGestionFCItemAsync().ContinueWith( x => {
+            App.Database.GetGestionFCItemAsync().ContinueWith(x => {
                 if (x.Result[0].UserSaved != 0)
                 {
                     UserRemember = x.Result[0].UserSaved;
@@ -103,22 +103,22 @@ namespace GestionFC
                     LogService logService = new LogService();
                     using (UserDialogs.Instance.Loading("Procesando...", null, null, true, MaskType.Black))
                     {
-                        await loginService.Login(loginModel).ContinueWith(async x =>
-                        { 
+                        await loginService.Login(loginModel).ContinueWith(x =>
+                        {
                             if (x.IsFaulted)
-                            {
                                 throw new Exception("Ocurrió un error");
-                            }
 
                             if (!x.Result.ResultadoEjecucion.EjecucionCorrecta)
-                            {
                                 throw new Exception(x.Result.ResultadoEjecucion.FriendlyMessage);
-                            }
 
                             if (!x.Result.UsuarioAutorizado)
-                            {
                                 throw new Exception("Usuario no autorizado.");
-                            }
+
+                            if (!x.Result.Activo)
+                                throw new Exception("Usuario inactivo.");
+
+                            if (!x.Result.EsGerente)
+                                throw new Exception("Usuario no cuenta con el perfil de gerente.");
 
 
                             //Guardamos la información en la base de datos SQL Lite
@@ -130,23 +130,22 @@ namespace GestionFC
                                 TokenSesion = x.Result.Token
                             };
 
-                            await App.Database.SaveGestionFCItemAsync(gestionFC);
+                            App.Database.SaveGestionFCItemAsync(gestionFC);
 
                             //Guardamos genramos la inserción en bitácora (inicio de sesión)
-                            var logModel = new LogSistemaModel() { 
-                                IdPantalla = 1, 
-                                IdAccion = 1, 
-                                Usuario = int.Parse(UserName.Text), 
-                                Dispositivo = DeviceInfo.Platform + DeviceInfo.Model + DeviceInfo.Name 
+                            var logModel = new LogSistemaModel()
+                            {
+                                IdPantalla = 1,
+                                IdAccion = 1,
+                                Usuario = int.Parse(UserName.Text),
+                                Dispositivo = DeviceInfo.Platform + DeviceInfo.Model + DeviceInfo.Name
                             };
-                            await logService.LogSistema(logModel, gestionFC.TokenSesion).ContinueWith( logRes =>
+                            logService.LogSistema(logModel, gestionFC.TokenSesion).ContinueWith(logRes =>
                             {
                                 if (logRes.IsFaulted)
                                 {
                                     throw logRes.Exception;
                                 }
-
-
                             });
 
                             // Navegamos hacia la pantalla plantilla que será la página principal de la aplicación
@@ -155,14 +154,15 @@ namespace GestionFC
                                 var plantillaPage = new PlantillaPage();
                                 Navigation.PushAsync(plantillaPage);
                             });
-
                         });
                     }
                 }
             }
             catch (Exception ex)
             {
+
                 await DisplayAlert("Error", ex.Message, "Ok");
+
             }
             finally
             {
