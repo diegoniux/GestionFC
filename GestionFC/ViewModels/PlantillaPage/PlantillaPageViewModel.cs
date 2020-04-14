515080e4e90a08c6111b08e9d141333346582a5f
Richi Garza <richi@conglom-osoft.net>
2020-04-14 11:42:12 -0500

    Cambios

--- GestionFC/ViewModels/PlantillaPage/PlantillaPageViewModel.cs
+++ GestionFC/ViewModels/PlantillaPage/PlantillaPageViewModel.cs
@@ -5,6 +5,11 @@ namespace GestionFC.ViewModels.PlantillaPage
 {
     public class PlantillaPageViewModel
     {
+        public string NombreGerente { get; set; }
+        public string Mensaje { get; set; }
+        public int Plantilla { get; set; }
+        public int APsMetaAlcanzada { get; set; }
+        public Models.PlantillaPage.Progreso Gerente { get; set; }
         public List<Models.PlantillaPage.Progreso> Agentes { get; set; }
         public PlantillaPageViewModel()
         {
