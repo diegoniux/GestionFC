7a43915f48da8b0f2f5b0ed78170b4a130525684
Richi Garza <richi@conglom-osoft.net>
2020-04-28 13:18:54 -0500

    Update LoginPage.xaml.cs
    bitacoras de error en login page

--- GestionFC/Views/LoginPage.xaml.cs
+++ GestionFC/Views/LoginPage.xaml.cs
@@ -94,6 +94,9 @@ namespace GestionFC
 
         async void btnIniciarSesion_Clicked(object sender, EventArgs e)
         {
+            // Llamamos el servicio para el login
+            LoginService loginService = new LoginService();
+            LogService logService = new LogService();
             try
             {
                 if (LoginViewModel.IsRunning)
@@ -105,10 +108,6 @@ namespace GestionFC
 
                     // construmos el objeto login que se validará
                     var loginModel = new LoginModel() { Nomina = int.Parse(UserName.Text), Password = PassworUser.Text };
-
-                    // Llamamos el servicio para el login
-                    LoginService loginService = new LoginService();
-                    LogService logService = new LogService();
                     using (UserDialogs.Instance.Loading("Procesando...", null, null, true, MaskType.Black))
                     {
                         // Temporal
@@ -197,8 +196,20 @@ namespace GestionFC
             }
             catch (Exception ex)
             {
+                var logError = new LogErrorModel()
+                {
+                    IdPantalla = 1,
+                    Usuario = int.Parse(UserName.Text),
+                    Error = ex.Message,
+                    Dispositivo = DeviceInfo.Platform + DeviceInfo.Model + DeviceInfo.Name
 
-                await DisplayAlert("Error", ex.Message, "Ok");
+                };
+                await logService.LogError(logError, "").ContinueWith(logRes =>
+                {
+                    if (logRes.IsFaulted)
+                        DisplayAlert("Error", logRes.Exception.Message, "Ok");
+                });
+                await DisplayAlert("Login Error", ex.Message, "Ok");
 
             }
             finally
