067156e8a579b7027167b865abb8810db05d1b75
Richi Garza <richi@conglom-osoft.net>
2020-06-17 17:55:42 -0500

    RankingPage

---
+++ GestionFC/ViewModels/RankingPage/RankingPageViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using GestionFC.Models.Ranking;
+using GestionFC.Models.Share;
+using GestionFC.ViewModels.Share;
+
+namespace GestionFC.ViewModels.RankingPage
+{
+    public class RankingViewModel: ViewModelBase
+    {
+        private string _nombreGerente;
+        public string NombreGerente
+        {
+            get { return _nombreGerente; }
+            set { _nombreGerente = value;
+                RaisePropertyChanged(nameof(NombreGerente));
+            }
+        }
+
+        private string _mensaje;
+
+        public string Mensaje
+        {
+            get { return _mensaje; }
+            set
+            {
+                _mensaje = value;
+                RaisePropertyChanged(nameof(Mensaje));
+            }
+        }
+
+        private int _plantilla;
+
+        public int Plantilla
+        {
+            get { return _plantilla; }
+            set
+            {
+                _plantilla = value;
+                RaisePropertyChanged(nameof(Plantilla));
+            }
+        }
+
+        private int _aPsMetaAlcanzada;
+
+        public int APsMetaAlcanzada
+        {
+            get { return _aPsMetaAlcanzada; }
+            set
+            {
+                _aPsMetaAlcanzada = value;
+                RaisePropertyChanged(nameof(APsMetaAlcanzada));
+            }
+        }
+
+        private Progreso _gerente;
+
+        public Progreso Gerente
+        {
+            get { return _gerente; }
+            set
+            {
+                _gerente = value;
+                RaisePropertyChanged(nameof(Gerente));
+            }
+        }
+
+        private List<RankingAP> _agentesTop;
+
+        public List<RankingAP> AgentesTop {
+            get { return _agentesTop; }
+            set { _agentesTop = value;
+                RaisePropertyChanged(nameof(AgentesTop));
+            }
+        }
+
+        private List<RankingAP> _agentes;
+
+        public List<RankingAP> Agentes
+        {
+            get { return _agentes; }
+            set
+            {
+                _agentes = value;
+                RaisePropertyChanged(nameof(Agentes));
+            }
+        }
+
+        public RankingViewModel()
+        {
+            this.AgentesTop = new List<RankingAP>();
+            this.Agentes = new List<RankingAP>();
+        }
+    }
+}
---
+++ GestionFC/Views/RankingPage.xaml.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Threading.Tasks;
+using Acr.UserDialogs;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using Service = GestionFC.Services;
+using Models = GestionFC.Models.Ranking;
+using GestionFC.Models.Log;
+
+namespace GestionFC.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RankingPage : ContentPage
+    {
+        public ViewModels.RankingPage.RankingViewModel ViewModel { get; set; }
+        private int nomina { get; set; }
+        private string token { get; set; }
+        public Master _master;
+        public bool SesionExpired { get; set; }
+
+        public RankingPage()
+        {
+            InitializeComponent();
+            SesionExpired = false;
+
+            _master = (Master)App.MasterDetail.Master;
+
+            ViewModel = new ViewModels.RankingPage.RankingViewModel();
+            BindingContext = ViewModel;
+            LoadPage();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            var burguerTap = new TapGestureRecognizer();
+            burguerTap.Tapped += (object sender, EventArgs e) =>
+            {
+                App.MasterDetail.IsPresented = !App.MasterDetail.IsPresented;
+            };
+            btnHamburguesa.GestureRecognizers.Add(burguerTap);
+
+        }
+
+        private async void LoadPage()
+        {
+            //logService = new Service.LogService();
+            Service.HeaderService headerService = new Service.HeaderService();
+            Service.PlantillaService gridPromotoresService = new Service.PlantillaService();
+            try
+            {
+                nomina = App.Nomina;
+                token = App.Token;
+
+                if (nomina > 0)
+                {
+                    using (UserDialogs.Instance.Loading("Procesando...", null, null, true, MaskType.Black))
+                    {
+                        await headerService.GetHeader(nomina, token).ContinueWith(x =>
+                        {
+                            if (x.IsFaulted)
+                            {
+                                throw x.Exception;
+                            }
+
+                            if (!x.Result.ResultadoEjecucion.EjecucionCorrecta)
+                            {
+
+                                // verificamos si la sesión expiró (token)
+                                if (x.Result.ResultadoEjecucion.ErrorMessage.Contains("401"))
+                                {
+                                    SesionExpired = true;
+                                    throw new Exception(x.Result.ResultadoEjecucion.FriendlyMessage);
+                                }
+                            }
+                            //Cargar datos para el binding de información con el header
+                            ViewModel.NombreGerente = x.Result.Progreso?.Nombre + " " + x.Result.Progreso?.Apellidos;
+                            ViewModel.Mensaje = x.Result.Progreso?.Genero == "H" ? "¡Bienvenido!" : "¡Bienvenida!";
+                            ViewModel.APsMetaAlcanzada = x.Result.APsMetaAlcanzada;
+                            ViewModel.Plantilla = x.Result.Plantilla;
+                            if (x.Result.Progreso != null)
+                            {
+                                ViewModel.Gerente = x.Result.Progreso;
+                            }
+
+                            ViewModel.AgentesTop.Add(new Models.Ranking.RankingAP
+                            {
+                                Nombre = "Victoria Janeth",
+                                Apellidos = "Rosas Camacho",
+                                Foto = "capi_circulo.png",
+                                Posicion = "1er LUGAR",
+                                Saldo = "$10,000,000",
+                                TipoSaldo = "",
+                                NumTraspaso = "10",
+                                ImgPosicionSemAnt = "",
+                                ColorPosicion = "#D5A73A",
+                                Estrellas = new Models.Ranking.RankEstrellas
+                                {
+                                    semana1 = "icon_star_gold.png",
+                                    semana2 = "icon_star_gold.png",
+                                    semana3 = "icon_star_gold.png",
+                                    semana4 = "icon_star_gold.png"
+                                }
+                            });
+                            ViewModel.AgentesTop.Add(new Models.Ranking.RankingAP
+                            {
+                                Nombre = "Domingo Javier",
+                                Apellidos = "Quintero Espinoza",
+                                Foto = "capi_circulo.png",
+                                Posicion = "2do LUGAR",
+                                Saldo = "$8,000,000",
+                                TipoSaldo = "",
+                                NumTraspaso = "8",
+                                ImgPosicionSemAnt = "",
+                                ColorPosicion = "#707070",
+                                Estrellas = new Models.Ranking.RankEstrellas
+                                {
+                                    semana1 = "icon_star_plate.png",
+                                    semana2 = "icon_star_plate.png",
+                                    semana3 = "icon_star_plate.png",
+                                    semana4 = "icon_star_grey.png"
+                                }
+                            });
+                            ViewModel.AgentesTop.Add(new Models.Ranking.RankingAP
+                            {
+                                Nombre = "Marco Antonio",
+                                Apellidos = "Pérez",
+                                Foto = "capi_circulo.png",
+                                Posicion = "3er LUGAR",
+                                Saldo = "$5,000,000",
+                                TipoSaldo = "",
+                                NumTraspaso = "6",
+                                ImgPosicionSemAnt = "",
+                                ColorPosicion = "#501313",
+                                Estrellas = new Models.Ranking.RankEstrellas
+                                {
+                                    semana1 = "icon_star_bronze.png",
+                                    semana2 = "icon_star_bronze.png",
+                                    semana3 = "icon_star_bronze.png",
+                                    semana4 = "icon_star_grey.png"
+                                }
+                            });
+                            ViewModel.Agentes.Add(new Models.Ranking.RankingAP
+                            {
+                                Nombre = "Rene Alexander",
+                                Apellidos = "Hernández de la Rosa",
+                                Foto = "capi_circulo.png",
+                                Posicion = "4 POSICIÓN",
+                                Saldo = "$4,000,000",
+                                TipoSaldo = "SALDO VIRTUAL",
+                                NumTraspaso = "4",
+                                ImgPosicionSemAnt = "icon_rank_up_green.png",
+                                ColorPosicion = "",
+                                Estrellas = new Models.Ranking.RankEstrellas
+                                {
+                                    semana1 = "icon_star_plate.png",
+                                    semana2 = "icon_star_bronze.png",
+                                    semana3 = "icon_star_gold.png",
+                                    semana4 = "icon_star_grey.png"
+                                }
+                            });
+                            ViewModel.Agentes.Add(new Models.Ranking.RankingAP
+                            {
+                                Nombre = "Mario Alberto",
+                                Apellidos = "Villarreal",
+                                Foto = "capi_circulo.png",
+                                Posicion = "5",
+                                Saldo = "$4,000,000",
+                                TipoSaldo = "SALDO VIRTUAL",
+                                NumTraspaso = "4",
+                                ImgPosicionSemAnt = "icon_rank_equal.png",
+                                ColorPosicion = "",
+                                Estrellas = new Models.Ranking.RankEstrellas
+                                {
+                                    semana1 = "icon_star_bronze.png",
+                                    semana2 = "icon_star_grey.png",
+                                    semana3 = "icon_star_bronze.png",
+                                    semana4 = "icon_star_grey.png"
+                                }
+                            });
+
+                            ViewModel.Agentes.Add(new Models.Ranking.RankingAP
+                            {
+                                Nombre = "José Antonio",
+                                Apellidos = "Rodríguez",
+                                Foto = "capi_circulo.png",
+                                Posicion = "6",
+                                Saldo = "$4,000,000",
+                                TipoSaldo = "SALDO VIRTUAL",
+                                NumTraspaso = "4",
+                                ImgPosicionSemAnt = "icon_rank_equal.png",
+                                ColorPosicion = "",
+                                Estrellas = new Models.Ranking.RankEstrellas
+                                {
+                                    semana1 = "icon_star_bronze.png",
+                                    semana2 = "icon_star_grey.png",
+                                    semana3 = "icon_star_bronze.png",
+                                    semana4 = "icon_star_grey.png"
+                                }
+                            });
+
+                            ViewModel.Agentes.Add(new Models.Ranking.RankingAP
+                            {
+                                Nombre = "José Luis",
+                                Apellidos = "Garcia",
+                                Foto = "capi_circulo.png",
+                                Posicion = "7",
+                                Saldo = "$4,000,000",
+                                TipoSaldo = "SALDO VIRTUAL",
+                                NumTraspaso = "4",
+                                ImgPosicionSemAnt = "icon_rank_down_red.png",
+                                ColorPosicion = "",
+                                Estrellas = new Models.Ranking.RankEstrellas
+                                {
+                                    semana1 = "icon_star_bronze.png",
+                                    semana2 = "icon_star_grey.png",
+                                    semana3 = "icon_star_bronze.png",
+                                    semana4 = "icon_star_grey.png"
+                                }
+                            });
+
+                            ViewModel.Agentes.Add(new Models.Ranking.RankingAP
+                            {
+                                Nombre = "José Saul",
+                                Apellidos = "Torres",
+                                Foto = "capi_circulo.png",
+                                Posicion = "8",
+                                Saldo = "$3,400,000",
+                                TipoSaldo = "SALDO VIRTUAL",
+                                NumTraspaso = "4",
+                                ImgPosicionSemAnt = "icon_rank_down_red.png",
+                                ColorPosicion = "",
+                                Estrellas = new Models.Ranking.RankEstrellas
+                                {
+                                    semana1 = "icon_star_bronze.png",
+                                    semana2 = "icon_star_grey.png",
+                                    semana3 = "icon_star_bronze.png",
+                                    semana4 = "icon_star_grey.png"
+                                }
+                            });
+
+                            ViewModel.Agentes.Add(new Models.Ranking.RankingAP
+                            {
+                                Nombre = "Micaela",
+                                Apellidos = "Duarte Gaitán",
+                                Foto = "capi_circulo.png",
+                                Posicion = "9",
+                                Saldo = "$3,000,000",
+                                TipoSaldo = "SALDO VIRTUAL",
+                                NumTraspaso = "5",
+                                ImgPosicionSemAnt = "icon_rank_down_red.png",
+                                ColorPosicion = "",
+                                Estrellas = new Models.Ranking.RankEstrellas
+                                {
+                                    semana1 = "icon_star_bronze.png",
+                                    semana2 = "icon_star_grey.png",
+                                    semana3 = "icon_star_grey.png",
+                                    semana4 = "icon_star_grey.png"
+                                }
+                            });
+
+                            ViewModel.Agentes.Add(new Models.Ranking.RankingAP
+                            {
+                                Nombre = "Erika Sarahi",
+                                Apellidos = "Guajardo Martínez",
+                                Foto = "capi_circulo.png",
+                                Posicion = "10",
+                                Saldo = "$2,000,000",
+                                TipoSaldo = "SALDO VIRTUAL",
+                                NumTraspaso = "2",
+                                ImgPosicionSemAnt = "icon_rank_down_red.png",
+                                ColorPosicion = "",
+                                Estrellas = new Models.Ranking.RankEstrellas
+                                {
+                                    semana1 = "icon_star_grey.png",
+                                    semana2 = "icon_star_grey.png",
+                                    semana3 = "icon_star_grey.png",
+                                    semana4 = "icon_star_grey.png"
+                                }
+                            });
+                        });
+                        
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                // Si la sesión expiró enviamos mensaje 
+                if (SesionExpired)
+                {
+                    CerrarSesion();
+                    return;
+                }
+
+                var logError = new Models.Log.LogErrorModel()
+                {
+                    IdPantalla = 2,
+                    Usuario = nomina,
+                    Error = (ex.TargetSite == null ? "" : ex.TargetSite.Name + ". ") + ex.Message,
+                    Dispositivo = DeviceInfo.Platform + DeviceInfo.Model + DeviceInfo.Name
+
+                };
+                await _master.logService.LogError(logError, "").ContinueWith(logRes =>
+                {
+                    if (logRes.IsFaulted)
+                        DisplayAlert("Error", logRes.Exception.Message, "Ok");
+                });
+                await DisplayAlert("RankingPage Error", ex.Message, "Ok");
+            }
+        }
+
+        void OnTapImageProductividad_Tapped(System.Object sender, System.EventArgs e)
+        {
+            // Navegamos hacia la pantalla plantilla que será la página principal de la aplicación
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                App.MasterDetail.Detail.Navigation.PushAsync((Page)Activator.CreateInstance(typeof(PlantillaPage)));
+                App.MasterDetail.IsPresented = false;
+            });
+        }
+
+        void OnTapMenuHamburguesa_Tapped(System.Object sender, System.EventArgs e)
+        {
+            App.MasterDetail.IsPresented = !App.MasterDetail.IsPresented;
+        }
+
+
+        async void CerrarSesion()
+        {
+            await DisplayAlert("Sesión Expirada.", "La sesión expiró, favor de ingresar nuevamente", "Ok");
+            // Navegamos hacia la pantalla plantilla que será la página principal de la aplicación
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                App.MasterDetail.Detail.Navigation.PushAsync((Page)Activator.CreateInstance(typeof(LoginPage)));
+                App.MasterDetail.IsPresented = false;
+            });
+        }
+    }
+}
