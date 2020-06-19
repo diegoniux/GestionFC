dae5edb074f2492900a213405d09e0b8078fe7d6
Richi Garza <richi@conglom-osoft.net>
2020-06-18 23:33:34 -0500

    Ranking service

---
+++ GestionFC/Models/Ranking/RankingResponseModel.cs
@@ -0,0 +1,17 @@
+﻿using GestionFC.Models.Share;
+using System;
+using System.Collections.Generic;
+
+namespace GestionFC.Models.Ranking
+{
+    public class RankingResponseModel
+    {
+        public ResultadoEjecucion ResultadoEjecucion { get; set; }
+        public List<RankingAP> TopGerentes { get; set; }
+        public List<RankingAP> Gerentes { get; set; }
+        public int PosicionDireccion { get; set; }
+        public string ImgPosicionSemAntDireccion { get; set; }
+        public int PosicionNacional { get; set; }
+        public string ImgPosicionSemAntNacional { get; set; }
+    }
+}
