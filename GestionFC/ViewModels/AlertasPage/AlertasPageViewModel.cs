60f3d55d273a625854a523caee84a13b5e67c1f9
CamiGrim <cammilo.grimmaldo@hotmail.com>
2020-07-05 22:02:39 -0500

    Update AlertasPageViewModel.cs
    tolower en apellido

--- GestionFC/ViewModels/AlertasPage/AlertasPageViewModel.cs
+++ GestionFC/ViewModels/AlertasPage/AlertasPageViewModel.cs
@@ -447,7 +447,7 @@ namespace GestionFC.ViewModels.AlertasPage
                 if (ejec)
                 {
                     ejec = false;
-                    var filteredItems = Secundarysource.Where(nombre => (nombre.Nombre.ToLower() + " " + (nombre.Apellidos ?? "")).Contains(filter.ToLower())).ToList();
+                    var filteredItems = Secundarysource.Where(nombre => (nombre.Nombre.ToLower() + " " + (nombre.Apellidos.ToLower() ?? "")).Contains(filter.ToLower())).ToList();
                     if (filter == "TODOS")
                     {
                         foreach (var nombre in Secundarysource)
