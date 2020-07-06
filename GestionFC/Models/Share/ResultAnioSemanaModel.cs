05ec59816fa0c787684b290973c0494f34215ca4
Richi Garza <richi@conglom-osoft.net>
2020-07-05 19:31:55 -0500

    total

--- GestionFC/Models/Share/ResultAnioSemanaModel.cs
+++ GestionFC/Models/Share/ResultAnioSemanaModel.cs
@@ -21,5 +21,47 @@ namespace GestionFC.Models.Share
 
         [JsonProperty("esUltimaFechaCorte")]
         public bool EsUltimaFechaCorte { get; set; }
+
+        [JsonProperty("totalSaldoVirtualLunes")]
+        public int TotalSaldoVirtualLunes { get; set; }
+
+        [JsonProperty("totalSaldoVirtualMartes")]
+        public int TotalSaldoVirtualMartes { get; set; }
+
+        [JsonProperty("totalSaldoVirtualMiercoles")]
+        public int TotalSaldoVirtualMiercoles { get; set; }
+
+        [JsonProperty("totalSaldoVirtualJueves")]
+        public int TotalSaldoVirtualJueves { get; set; }
+
+        [JsonProperty("totalSaldoVirtualViernes")]
+        public int TotalSaldoVirtualViernes { get; set; }
+
+        [JsonProperty("totalSaldoVirtualSabado")]
+        public int TotalSaldoVirtualSabado { get; set; }
+
+        [JsonProperty("totalSaldoVirtualDomingo")]
+        public int TotalSaldoVirtualDomingo { get; set; }
+
+        [JsonProperty("totalSaldoVirtualSemana")]
+        public int TotalSaldoVirtualSemana { get; set; }
+
+        [JsonProperty("totalSaldoVirtualFaltante")]
+        public int TotalSaldoVirtualFaltante { get; set; }
+
+        [JsonProperty("totalMetaSemana")]
+        public int TotalMetaSemana { get; set; }
+
+        [JsonProperty("totalFCTInactivos")]
+        public int TotalFCTInactivos { get; set; }
+
+        [JsonProperty("totalFCTActivos")]
+        public int TotalFCTActivos { get; set; }
+
+        [JsonProperty("totalFoliosMenores30k")]
+        public int TotalFoliosMenores30k { get; set; }
+
+        [JsonProperty("totalFoliosCertificados")]
+        public int TotalFoliosCertificados { get; set; }
     }
 }
